6bca2bd347c964bbf0ba59e385afc8a632d43d9d
amirtavana117 <amirmahdirezaetavana@gmail.com>
2022-07-06 10:29:36 +0430

    Creat Car Class

---
+++ Car.py
@@ -0,0 +1,10 @@
+COLORS = ["Red", "orange", "yellow", "green", "blue", "purple"]
+
+STARTING_MOVE_DISTANCE = 5
+
+MOVE_FORWARD = 10
+
+
+class Car:
+    def __init__(self) -> None:
+        pass
--- main.py
+++ main.py
@@ -1,20 +1,17 @@
-import time 
+import time
 from turtle import Screen
 
 
-
-
 screen = Screen()
-screen.setup(width=600,height=600)
+screen.setup(width=600, height=600)
 screen.tracer(0)
 
 
-Game_is_on =True 
+Game_is_on = True
 
 while Game_is_on:
     time.sleep(0.1)
     screen.update()
-
 
 
 screen.exitonclick()
