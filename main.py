c5636bea74677a680ebb28610a48a826563e7c10
amirtavana117 <amirmahdirezaetavana@gmail.com>
2022-07-06 13:05:54 +0430

    Add ScoreBoard Class

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import time
 from turtle import Screen
 from player import Player
 from Car import Car
-
+from scoreBoard import ScoreBoard
 screen = Screen()
 screen.tracer(0)
 screen.setup(width=600, height=600)
@@ -10,6 +10,7 @@ screen.setup(width=600, height=600)
 
 player = Player()
 car = Car()
+scoreboard = ScoreBoard()   
 screen.listen()
 screen.onkey(player.Up, "Up")
 
@@ -18,12 +19,16 @@ Game_is_on = True
 while Game_is_on:
     time.sleep(0.1)
     screen.update()
+    scoreboard.update_scordboard()
     car.Create_car()
     car.Move()
     for car_r in car.Cars:
         if car_r.distance(player) < 20:
+            scoreboard.Game_over()
             Game_is_on = False
     if player.win(player.ycor()):
         car.Increase_speed()
+        scoreboard.increase_score()
+
 
 screen.exitonclick()
--- scoreBoard.py
+++ scoreBoard.py
@@ -1,5 +1,26 @@
+from turtle import Turtle
+
+
 FONT = ("Courier", 24, "normal")
 
-class ScoreBoard:
-    def __init__(self) -> None:
-        pass
+
+class ScoreBoard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.color("black")
+        self.hideturtle()
+        self.penup()
+        self.score = 0
+        self.update_scordboard()
+
+    def update_scordboard(self):
+        self.clear()
+        self.goto(-200, 200)
+        self.write(f"Score:{self.score}", align="center", font=FONT)
+
+    def increase_score(self):
+        self.score += 1
+
+    def Game_over(self):
+        self.goto(0, 0)
+        self.write("Game over", align="center", font=FONT)
