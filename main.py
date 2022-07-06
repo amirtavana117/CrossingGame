439a56d4519d9a125c280a632292e1e07ad646e0
amirtavana117 <amirmahdirezaetavana@gmail.com>
2022-07-06 09:48:43 +0430

    Create Main File

---
+++ main.py
@@ -0,0 +1,20 @@
+import time 
+from turtle import Screen
+
+
+
+
+screen = Screen()
+screen.setup(width=600,height=600)
+screen.tracer(0)
+
+
+Game_is_on =True 
+
+while Game_is_on:
+    time.sleep(0.1)
+    screen.update()
+
+
+
+screen.exitonclick()
